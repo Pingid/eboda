4344f337fc4067321cc7f6978cdce6ebf401964b
Dan Beaven <dm.beaven@gmail.com>
2019-12-21 18:04:35 +0000

    Working upload but files get corrupted

--- functions/handler.ts
+++ functions/handler.ts
@@ -1,12 +1,53 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
+import { APIGatewayProxyHandler, APIGatewayProxyEvent } from "aws-lambda";
+import "source-map-support/register";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-}
+import { createWriteStream } from "fs";
+import { join } from "path";
+
+import busboy from "busboy";
+
+const writeFileBB = (
+  event: APIGatewayProxyEvent
+): Promise<{
+  filename: string;
+  encoding: string;
+  mimetype: string;
+}> => {
+  const bb = new busboy({
+    headers: { ...event.headers, "content-type": event.headers["Content-Type"] }
+  });
+
+  return new Promise((resolve, reject) => {
+    let res;
+
+    bb.on("file", function(_fieldname, file, filename, encoding, mimetype) {
+      const saveTo = join("./tmp", filename);
+      res = { filename, encoding, mimetype };
+      console.log(res);
+      let stream = createWriteStream(saveTo);
+      file.pipe(stream);
+      stream.on("close", () => {
+        resolve(res);
+      });
+    });
+    bb.on("error", reject);
+    bb.end(event.body);
+  });
+};
+
+export const upload: APIGatewayProxyHandler = async (event, _context) =>
+  Promise.resolve()
+    .then(() => writeFileBB(event))
+    .then(file => {
+      const { filename } = file;
+      console.log(filename);
+      return filename;
+    })
+    .then(() => ({
+      statusCode: 200,
+      body: JSON.stringify({
+        message:
+          "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
+        input: event
+      })
+    }));
