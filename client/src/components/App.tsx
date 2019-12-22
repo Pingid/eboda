6af585defd103b86ae24385673bf8b6cdb02d70f
Dan Beaven <dm.beaven@gmail.com>
2019-12-22 00:17:44 +0000

    Styled the clientside

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,28 +1,31 @@
 import React, { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
 import useFileUpload from '../utils/useFileUpload';
+import { Helmet } from 'react-helmet'
+
+const HideMe = ({ visible, children }: { visible: boolean, children: React.ReactNode }) => <div className={'hide ' + (visible ? '' : 'me')}>{children}</div>
 
 const App = () => {
   const { progress, network, upload, url } = useFileUpload();
 
   const onDrop = useCallback(acceptedFiles => upload(acceptedFiles[0]), [upload])
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, multiple: false })
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, multiple: false, disabled: network !== 'ready' })
 
   return (
-    <div style={{ width: '100%', height: '100%' }}>
-      <div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }} {...getRootProps()}>
-        <input {...getInputProps()} />
-        {
-          isDragActive ?
-            <p>Drop the files here ...</p> :
-            <p>Drag 'n' drop some files here, or click to select files</p>
-        }
-        <div>
-          <h3>Network: {network}</h3>
-          {progress !== 0 && <h1>{progress}</h1>}
-          {network === 'done' && url && <a href={url}>download</a>}
-        </div>
-      </div>
+    <div className="wrapper" {...getRootProps()}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>eboda</title>
+        <meta name="description" content="Fix 'The project was saved in a newer version' broken adobe premier, illustrator, photoshop ... files "></meta>
+      </Helmet>
+      <p className="about">fix adobe files that wont open on older software versions</p>
+      <input {...getInputProps()} />
+
+      <HideMe visible={network === 'ready' && isDragActive}><h2>Drop the file here</h2></HideMe>
+      <HideMe visible={network === 'ready' && !isDragActive}><h2>drop your file here or <button className="button link">click</button> to find</h2></HideMe>
+      <HideMe visible={network === 'processing' || network === 'requestURL' || network === 'uploading'}><h1>{progress}<span style={{ fontSize: '5vw' }}>%</span></h1></HideMe>
+      <HideMe visible={network === 'error'}><h2 className="">something went wrong try again later</h2></HideMe>
+      <HideMe visible={network === 'done'}><h2 className=""><a href={url as string} className="link">download</a></h2></HideMe>
     </div>
   )
 }
